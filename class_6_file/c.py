9a13d3a681da24cb8b490b9c8a8ab52145f09c85
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-05-22 01:40:17 +0800

    作业

---
+++ class_6_file/c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=UTF-8
+"""
+@Author: STAURL.COM
+@Contact: admin@staurl.com
+@Project: python_full_stack_automation_test
+@File: c.py
+@Time: 2019/5/22 1:40
+@Desc: S
+"""
