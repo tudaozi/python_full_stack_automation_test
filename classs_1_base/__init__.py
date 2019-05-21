6ddc546e543375f8adb44c00b4feb80a8804d5f1
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-05-21 22:05:17 +0800

    练习

---
+++ classs_1_base/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=UTF-8
+"""
+@Author: STAURL.COM
+@Contact: admin@staurl.com
+@Project: python_full_stack_automation_test
+@File: __init__.py.py
+@Time: 2019/5/21 19:56
+@Desc: S
+"""
---
+++ classs_1_base/intde.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding=UTF-8
+"""
+@Author: STAURL.COM
+@Contact: admin@staurl.com
+@Project: python_full_stack_automation_test
+@File: intde.py
+@Time: 2019/5/21 20:10
+@Desc: S
+"""
+a = 1
+print(type(a))
+b = "asdfasd'd'"
