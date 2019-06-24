60b32554530e485fd95c1e3c57e2aaaa440d9927
Tudaozi <1626202029@qq.com>
2019-06-25 03:00:18 +0800

    练习

--- python_basics/class_4_function_scope/pack2/module3.py
+++
@@ -1,9 +0,0 @@
-# # 1
-# from class_4_function.pack1.module1 import mod1
-#
-# mod1()
-#############
-
-import python_basics.class_4_function_scope.pack1.module1 as mod
-
-mod.mod1()
---
+++ python_basics/class_6_function_scope/pack2/module3.py
@@ -0,0 +1,9 @@
+# # 1
+# from class_5_function.pack1.module1 import mod1
+#
+# mod1()
+#############
+
+import python_basics.class_6_function_scope.pack1.module1 as mod
+
+mod.mod1()
