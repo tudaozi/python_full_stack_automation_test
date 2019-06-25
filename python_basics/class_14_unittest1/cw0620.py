87fe09d874efd7a0f823699560a0b8ff8e46341f
Tudaozi <1626202029@qq.com>
2019-06-26 01:16:32 +0800

    练习

---
+++ python_basics/class_14_unittest1/cw0620.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=UTF-8
+"""
+@Author: STAURL.COM
+@Contact: admin@staurl.com
+@Project: python_full_stack_automation_test
+@File: cw0620.py
+@Time: 2019-06-26 00:31
+@Desc: S
+"""
+from openpyxl import load_workbook, Workbook
+import unittest
+
+
+# 1、封装一个方法或者类，读取 excel 文件里面的数据
+class OneExcel:
+    def __init__(self, path):
+        self.path = path
+        pass
+
+    def open_excel(self):
+        wb = Workbook()
+        ws = wb.active
+        ws.append(range(10))
+        wd = tuple(ws.iter_rows(values_only=True))
+        wd1 = ''
+        for i in wd[0]:
+            wd1 += str(eval('i'))
+        wb.save(self.path)
+        print(wd1)
+
+
+my_excel = OneExcel('demo.xlsx')
+my_excel.open_excel()
+
+
+# 2、测试用例1：断言文件 第2行 第三列是否为空
+class TestExcel(unittest.TestCase):
+    def __init__(self, methodname, path):
+        super().__init__(methodName=methodname)
+        self.path = path
+
+    def setUp(self):
+        wb = load_workbook(self.path)
+        ws = wb.active
+        wd = ws.cell(2, 3).value
+        wd1 = ws.cell(1, 2).value
+        return wd, wd1
+
+    def TestNone(self):
+        self.assertAlmostEqual(self.setUp()[0], None)
+
+    # 3、测试用例2：断言文件 第1行 第二列是否和长沙相等。
+
+    def TestEqual(self):
+        self.assertAlmostEqual(self.setUp()[1], '长沙')
