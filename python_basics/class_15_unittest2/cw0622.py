582127b209dd86f2891e23351d8afdf4816cb477
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-06-26 21:02:02 +0800

    作业

---
+++ python_basics/class_15_unittest2/cw0622.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=UTF-8
+"""
+@Author: STAURL.COM
+@Contact: admin@staurl.com
+@Project: python_full_stack_automation_test
+@File: cw0622.py
+@Time: 2019/6/26 9:03
+@Desc: S
+"""
+
+# 将上节课的单元测试用例通过 suite 和 runner 执行
+from ../class_14_unittest1/cw0620.py
+# 1、两种以上 suite 添加测试用例的方式。（loader)
+# 2、使用 HTMLTestRunner 生成测试报告
