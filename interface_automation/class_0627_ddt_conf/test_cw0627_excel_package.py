1a6583d7898df2e18466a6c42df82b215189184b
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-07-02 22:17:34 +0800

    作业

--- interface_automation/class_0627_ddt_conf/test_cw0627_excel_package.py
+++ interface_automation/class_0627_ddt_conf/test_cw0627_excel_package.py
@@ -25,30 +25,26 @@ class HandleExcel:  # 创建一个Excel处理类
         return wb, ws
 
     def get_case(self):  # 获取行-测试用例
+        wb, ws = self.load_excel()
         cases_list = []  # 创建一个空的用例列表
-        cases_title_entity = tuple(self.load_excel()[1].iter_rows(max_row=1, values_only=True))
+        cases_title_entity = tuple(ws.iter_rows(max_row=1, values_only=True))
         cases_title = cases_title_entity[0]
-        for cases_entity in tuple(self.load_excel()[1].iter_rows(min_row=2, values_only=True)):
+        for cases_entity in tuple(ws.iter_rows(min_row=2, values_only=True)):
             cases_list.append(dict(zip(cases_title, cases_entity)))
+        wb.close()
         return cases_list  # 将用例列表返回给当前函数
 
-    # def write_result(self, row, actual, result):
-    # if isinstance(row, int) and (2 <= row <= self.load_excel()[1].max_row):
-    #     # self.load_excel()[1].cell(row=row, column=6, value=actual)
-    #     # self.load_excel()[1].cell(row=row, column=7, value=result)
-    #     self.load_excel()[1].cell(3, 6).value == actual
-    #     self.load_excel()[1].cell(3, 7).value = result
-    #     print(self.load_excel()[1].cell(3, 7).value)
-    #     print(self.load_excel()[1])
-    #     self.load_excel()[0].save(self.filename)
-    def write_result(self, actual, result):
-        self.load_excel()[1].cell(1, 6, value=actual)
-        self.load_excel()[1].cell(1, 7, value=result)
-        self.load_excel()[0].save(self.filename)
+    def write_result(self, row, actual, result):
+        wb, ws = self.load_excel()
+        if isinstance(row, int) and (2 <= row <= ws.max_row):
+            ws.cell(row=row, column=6, value=actual)
+            ws.cell(row=row, column=7, value=result)
+            wb.save(self.filename)
+            wb.close()
 
 
 if __name__ == '__main__':  # 在当前路径下才可以运行
     file_name = 'demo.xlsx'  # 指定文件
     my_excel = HandleExcel(file_name, 'add')  # 实例化my_excel这个对象
     print(my_excel.get_case())  # 将对象调用方法后的结果打印出来
-    my_excel.write_result(1, 2)
+    my_excel.write_result(2, 4, 5)
