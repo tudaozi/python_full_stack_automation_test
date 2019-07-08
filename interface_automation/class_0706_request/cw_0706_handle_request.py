3082ec5d6798fc1c8dd83f07b4feabcdbe56a8b3
Tudaozi <46038475+tudaozi@users.noreply.github.com>
2019-07-08 18:00:23 +0800

    作业

--- interface_automation/class_0706_request/cw_0706_handle_request.py
+++ interface_automation/class_0706_request/cw_0706_handle_request.py
@@ -9,16 +9,44 @@
 @Desc: S
 """
 import requests
+import json
+from interface_automation.class_0706_request.cw0706_log import do_logger
 
-# 1、构造请求的url
-url = 'http://test.lemonban.com:8080/futureloan/mvc/api/member/register'
-# 2、创建请求参数
-params = {
-    'mobilephone': 13798288888,
-    'pwd': 123456,
-    'regname': '刀刀'
-}
-# 向服务器发起GET请求
-res = requests.get(url, params=params)
 
-pass
+class HttpRequest:
+    def __init__(self):
+        self.one_session = requests.Session()
+
+    def send_request(self, method, url, data=None, is_json=False, **kwargs):
+        method = method.upper()
+        if isinstance(data, str):
+            try:
+                data = json.loads(data)
+            except Exception as e:
+                do_logger.error("将json转为Python中的数据类型时, 出现异常: {}".format(e))
+                data = eval(data)
+        if method == 'GET':
+            res = self.one_session.request(method=method, url=url, params=data, **kwargs)
+        elif method == 'POST':
+            if is_json:
+                res = self.one_session.request(method=method, url=url, json=data, **kwargs)
+            else:
+                res = self.one_session.request(method=method, url=url, data=data, **kwargs)
+        else:
+            res = None
+            do_logger.error("不支持【{}】方法请求".format(method))
+        return res
+
+    def request_close(self):
+        self.one_session.close()
+
+
+if __name__ == '__main__':
+    url = 'http://test.lemonban.com:8080/futureloan/mvc/api/member/register'
+    params = {
+        'mobilephone': 13798288888,
+        'pwd': 123456,
+        'regname': '刀刀'
+    }
+    my_httprequest = HttpRequest()
+    my_httprequest.send_request(method='GET', url=url, data=params)
