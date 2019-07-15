d2051b35f255cdf9fc06b0251b1f2a4c9ca8be79
Tudaozi <1626202029@qq.com>
2019-07-16 01:40:40 +0800

    作业

--- interface_automation/class_0709_mysql/cw_0709_case.py
+++ interface_automation/class_0709_mysql/cw_0709_case.py
@@ -8,19 +8,56 @@
 @Time: 2019-06-29 22:18
 @Desc: S
 """
-import unittest
+import json
 import re
+import unittest
+
 from ddt import ddt, data
 
 from interface_automation.class_0709_mysql.cw_0709_config import do_config
+from interface_automation.class_0709_mysql.cw_0709_excel import HandleExcel
 from interface_automation.class_0709_mysql.cw_0709_log import do_logger
 from interface_automation.class_0709_mysql.cw_0709_mysql import do_mysql
-from interface_automation.class_0709_mysql.cw_0709_excel import HandleExcel
+from interface_automation.class_0709_mysql.cw_0709_request import do_request
 
 # file = config['file path']['case_path']
 file = do_config.get_value('file path', 'case_path')
 true_result = do_config.get_value('msg', 'true_result')
 fail_result = do_config.get_value('msg', 'fail_result')
+
+
+def excel_suite():
+    not_existed_tel = do_mysql.not_existed_tel(do_config.get_value('mysql', 'not_existed_tel'))
+    register_excel = HandleExcel(do_config.get_value('file path', 'case_path'), 'register')
+    register_excel_cases = register_excel.get_case()
+    register_excel_re = re.sub(r'\${not_existed_tel}', not_existed_tel, str(register_excel_cases))
+    existed_tel = do_mysql.existed_tel(do_config.get_value('mysql', 'existed_tel'),
+                                       do_config.get_int('mysql', 'existed_tel_type'))
+    register_cases = re.sub(r'\${existed_tel}', existed_tel, str(register_excel_re))
+    register_cases = eval(register_cases)
+    wb, ws = register_excel.load_excel()
+    wb.close()
+    return register_cases
+
+
+cases_suite = excel_suite()
+
+
+def excel_suite1():
+    not_existed_tel = do_mysql.not_existed_tel(do_config.get_value('mysql', 'not_existed_tel'))
+    register_excel = HandleExcel(do_config.get_value('file path', 'case_path'), 'login')
+    register_excel_cases = register_excel.get_case()
+    register_excel_re = re.sub(r'\${not_existed_tel}', not_existed_tel, str(register_excel_cases))
+    existed_tel = do_mysql.existed_tel(do_config.get_value('mysql', 'existed_tel'),
+                                       do_config.get_int('mysql', 'existed_tel_type'))
+    register_cases = re.sub(r'\${existed_tel}', existed_tel, str(register_excel_re))
+    register_cases = eval(register_cases)
+    wb, ws = register_excel.load_excel()
+    wb.close()
+    return register_cases
+
+
+cases_suite1 = excel_suite1()
 
 
 @ddt
@@ -29,15 +66,13 @@ class ApiTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         do_logger.info('\n{:=^40s}\n'.format('开始执行用例'))
-        not_existed_tel=do_mysql.not_existed_tel(do_config.get_int('mysql','not_existed_tel'))
-        register=HandleExcel(file, 'register').get_case()
-        pass
 
     @classmethod
     def tearDownClass(cls):
         do_logger.info('\n{:=^40s}\n'.format('用例执行结束'))
 
     def setUp(self):
+
         pass
 
     def tearDown(self):
@@ -45,22 +80,49 @@ class ApiTest(unittest.TestCase):
         wb.save(file)
         wb.close()
 
-    @data(*HandleExcel(file, 'add').get_case())
-    def test_add(self, case_list):
-        self.my_HandleExcel = HandleExcel(file, 'add')
-        case_id, title, l_data, r_data, expected = case_list['case_id'], case_list['title'], case_list['l_data'], \
-                                                   case_list['r_data'], case_list['expected']
-        actual = ApiTest(l_data, r_data).add()
-        result = expected
-        msg = title
+    @data(*cases_suite)
+    def test_register(self, case_list):
+        self.my_HandleExcel = HandleExcel(file, 'register')
+        request_result = do_request.send_request(case_list['method'],
+                                                 do_config.get_value('request', 'default_address') + case_list[
+                                                     'url_path'],
+                                                 case_list['data'])
+        actual_text = request_result.text
+        actual_value = json.loads(actual_text)['code']
+        actual = 'code: "{}",'.format(actual_value)
+        result = case_list['expected']
+        msg = case_list['title']
         try:
             self.assertEqual(result, actual, msg)
             print('{},执行结果为:{}'.format(msg, true_result))
-            self.my_HandleExcel.write_result(case_id + 1, actual, true_result)
+            self.my_HandleExcel.write_result(case_list['case_id'] + 1, actual, true_result)
             do_logger.error("{}, 执行结果为: {}".format(msg, true_result))
         except AssertionError as e:
             print('具体异常为{}'.format(e))
-            self.my_HandleExcel.write_result(case_id + 1, actual, fail_result)
+            self.my_HandleExcel.write_result(case_list['case_id'] + 1, actual, fail_result)
+            do_logger.error("{}, 执行结果为: {},具体异常为{}".format(msg, fail_result, e))
+            raise e
+
+    @data(*cases_suite1)
+    def test_login(self, case_list):
+        self.my_HandleExcel = HandleExcel(file, 'login')
+        request_result = do_request.send_request(case_list['method'],
+                                                 do_config.get_value('request', 'default_address') + case_list[
+                                                     'url_path'],
+                                                 case_list['data'])
+        actual_text = request_result.text
+        actual_value = json.loads(actual_text)['code']
+        actual = 'code: "{}",'.format(actual_value)
+        result = case_list['expected']
+        msg = case_list['title']
+        try:
+            self.assertEqual(result, actual, msg)
+            print('{},执行结果为:{}'.format(msg, true_result))
+            self.my_HandleExcel.write_result(case_list['case_id'] + 1, actual, true_result)
+            do_logger.error("{}, 执行结果为: {}".format(msg, true_result))
+        except AssertionError as e:
+            print('具体异常为{}'.format(e))
+            self.my_HandleExcel.write_result(case_list['case_id'] + 1, actual, fail_result)
             do_logger.error("{}, 执行结果为: {},具体异常为{}".format(msg, fail_result, e))
             raise e
 
--- interface_automation/class_0709_mysql/test.py
+++ interface_automation/class_0709_mysql/test.py
@@ -8,17 +8,10 @@
 @Time: 2019/7/13 1:22
 @Desc: S
 """
-import pymysql
-import random
-import string
-import unittest
-import re
-from ddt import ddt, data
-from interface_automation.class_0709_mysql.cw_0709_config import do_config
-from interface_automation.class_0709_mysql.cw_0709_log import do_logger
-from interface_automation.class_0709_mysql.cw_0709_mysql import do_mysql
-from interface_automation.class_0709_mysql.cw_0709_excel import HandleExcel
+import json
 
+from interface_automation.class_0709_mysql.cw_0709_config import do_config
+from interface_automation.class_0709_mysql.cw_0709_request import do_request
 
 # def create_tel():
 #     tel_head = random.choice(['137', '138', '139'])
@@ -28,10 +21,49 @@ from interface_automation.class_0709_mysql.cw_0709_excel import HandleExcel
 #
 #
 # print(create_tel())
-def splice():
-    not_existed_tel = do_mysql.not_existed_tel(do_config.get_value('mysql', 'not_existed_tel'))
-    register = HandleExcel(do_config.get_value('file path', 'case_path'), 'register').get_case()
-    return not_existed_tel,register
+# def splice():
+#     not_existed_tel = do_mysql.not_existed_tel(do_config.get_value('mysql', 'not_existed_tel'))
+#     register_excel = HandleExcel(do_config.get_value('file path', 'case_path'), 'register')
+#     register_excel_cases = register_excel.get_case()
+#     register_excel_re = re.sub(r'\${not_existed_tel}', not_existed_tel, str(register_excel_cases))
+#     existed_tel = do_mysql.existed_tel(do_config.get_value('mysql', 'existed_tel'),
+#                                        do_config.get_int('mysql', 'existed_tel_type'))
+#     register_cases = re.sub(r'\${existed_tel}', existed_tel, str(register_excel_re))
+#     # register_cases = list(register_cases)
+#     wb, ws = register_excel.load_excel()
+#     wb.close()
+#
+#     return register_cases
+#
+#
+# cases_list = splice()
+# cases_list = eval(cases_list)
+case_list = {'case_id': 1, 'title': '使用不存在的手机号进行注册', 'url_path': '/member/register',
+             'data': '{"mobilephone": "13786245301", "pwd": 123456, "regname": "刀刀"}', 'method': 'POST',
+             'expected': 'code: "10001",', 'actual': None, 'result': None}
+# print(type(case_list),case_list)
+method = case_list['method']
+url = do_config.get_value('request', 'default_address') + case_list['url_path']
+datas = case_list['data']
+datas = eval(datas)
+
+# actual = do_request.send_request(case_list['method'],
+#                                         do_config.get_value('request', 'default_address') + case_list['url_path'],
+#                                         eval(case_list['data']))
+
+print(type(method), method)
+print(type(url), url)
+print(type(datas), datas)
+# print(actual)
 
 
-print(splice())
+request_result = do_request.send_request(case_list['method'],
+                                         do_config.get_value('request', 'default_address') + case_list['url_path'],
+                                         case_list['data'])
+actual_text = request_result.text
+actual_value = json.loads(actual_text)['code']
+actual = 'code: "{}",'.format(actual_value)
+do_request.request_close()
+result = case_list['expected']
+msg = case_list['title']
+print(actual)
